e1d78581581c1645432f25650a58c28b0fab93df
Emily Duerr <ejduerr@wisc.edu>
2012-08-08 15:57:48 -0500

    Skeleton rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Birds::Application.config.secret_token = '9272979c690725215db82391e514c12907aeff310c2db2b6b275574d50c626cca6a99dd3d6a9472667096d7774616cda9c58b809864bad1008a8aae61f761989'
