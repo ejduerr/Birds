fcd73592ee2e0745b3ba673b9eedf6eda2f3e611
Emily Duerr <ejduerr@wisc.edu>
2012-12-17 15:32:43 -0600

    add chapter photos to folder, attempt to add to site
    what did I break?
    also fill in some missing tibetan names

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,3 +1,5 @@
 class Chapter < ActiveRecord::Base
 	has_many :birds
+			
+		has_attached_file :primary_image, :styles => { :main => ["375x375#", :png], :thumb => ["140x140#", :png], :tiny => ["96x96#", :png]}
 end
