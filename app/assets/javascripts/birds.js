de579b6130a88dc94e10ce9702f9757ef40f3b6b
Emily Duerr <ejduerr@wisc.edu>
2013-01-29 17:05:49 -0600

    adding quick search, add bird list in chapter, add chapter names on homepage

--- app/assets/javascripts/birds.js
+++ app/assets/javascripts/birds.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
   $('.bxslider').bxSlider({auto: true});
   $( ".tabs" ).tabs();
+  
+	var atoz = $('#atoz');
+	$('input#q').quicksearch('#atoz ul li' , {
+		'delay': 100,
+		'selector': 'a',
+		'onAfter': function () {
+			var inputString = $('input', atoz).val();
+			if (inputString) {
+				$('li a', atoz).removeHighlight().highlight(inputString);
+			} else {
+				$('li a', atoz).removeHighlight();
+			}
+		}
+	});
+
 });
